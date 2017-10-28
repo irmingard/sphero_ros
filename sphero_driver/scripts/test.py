28d6cbbb987c608408bf0f69c35b95240154c784
wabion-veronika <veronika.priesner@wabion.at>
2017-10-28 15:46:09 +0200

    simplified test file

--- sphero_driver/scripts/test.py
+++ sphero_driver/scripts/test.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python
-import bluetooth
-import struct
-import time
+
 from sphero_driver import sphero_driver
-import sys
+from time import sleep
+
+# if you know your Sphero's address
+# sphero = sphero_driver.Sphero("Sphero", "FF:CD:AA:99:45:00")
 sphero = sphero_driver.Sphero()
-sphero.connect()
-sphero.set_raw_data_strm(40, 1 , 0, False)
 
-sphero.start()
-time.sleep(2)
-sphero.set_rgb_led(255,0,0,0,False)
-time.sleep(1)
-sphero.set_rgb_led(0,255,0,0,False)
-time.sleep(1)
-sphero.set_rgb_led(0,0,255,0,False)
-time.sleep(3)
-sphero.join()
-sphero.disconnect()
-sys.exit(1)
+while not sphero.is_connected:
+    sphero.connect()
 
-
-
+sleep(1)
+sphero.set_rgb_led(255, 0, 0, 0, False)
+sleep(1)
+sphero.set_rgb_led(0, 255, 0, 0, False)
+sleep(1)
+sphero.set_rgb_led(0, 0, 255, 0, False)
+sleep(1)
